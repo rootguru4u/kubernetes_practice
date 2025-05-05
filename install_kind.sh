77ae53c2e9c8829799a35d4af77366b09be09791
Shyamsundar Dattatraya Dudhbhate <shyam777100@gmail.com>
2025-05-05 10:06:50 +0530

    kind_installation script

---
+++ install_kind.sh
@@ -0,0 +1,11 @@
+# This script install KIND K8S server --> Kubernetes in docker
+
+#!/bin/bash
+
+# For AMD / x86_64
+
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
+chmod +x ./kind
+sudo cp ./kind /usr/local/bin/kind
+
+rm -rf kind
